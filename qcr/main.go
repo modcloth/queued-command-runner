87c4081c0f21993dbb77e6321b4f9d3cff260957
Rafe Colton <r.colton@modcloth.com>
2014-05-10 15:41:32 -0700

    Adding main, as it is currently functioning as the test script
    (even though `qcr` is intended to be used as a library)

--- qcr/main.go
+++ qcr/main.go
@@ -3,15 +3,74 @@ package main
 import "github.com/modcloth/queued-command-runner"
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 )
 
-func main() {
+import (
+	"github.com/onsi/gocleanup"
+)
 
-	for x := 0; x < 50; x++ {
-		cmd := exec.Command("ls", "/tmp")
+func main() {
+	//setup tmpdir
+	tmp, err := ioutil.TempDir("", "qcr")
+	if err != nil {
+		gocleanup.Exit(1)
+	}
+
+	gocleanup.Register(func() {
+		os.RemoveAll(tmp)
+	})
+
+	script := `#!/bin/bash
+
+echo begin >&2
+sleep 1
+echo end >&2
+`
+	script2 := `#!/bin/bash
+
+echo begin >&2
+sleep 2
+echo end >&2
+`
+	scriptPath := fmt.Sprintf("%s/foo", tmp)
+	scriptPath2 := fmt.Sprintf("%s/foo2", tmp)
+
+	if err := ioutil.WriteFile(scriptPath, []byte(script), 0755); err != nil {
+		fmt.Printf("ERR: %q\n", err)
+		gocleanup.Exit(3)
+	}
+
+	if err := ioutil.WriteFile(scriptPath2, []byte(script2), 0755); err != nil {
+		fmt.Printf("ERR: %q\n", err)
+		gocleanup.Exit(3)
+	}
+
+	for x := 0; x < 5; x++ {
+		cmd := &exec.Cmd{
+			Path:   scriptPath,
+			Args:   []string{"foo"},
+			Stdout: os.Stdout,
+		}
+
 		qcr.Run(cmd)
 	}
-	os.Exit(0)
+
+	for x := 0; x < 5; x++ {
+		cmd := &exec.Cmd{
+			Path:   scriptPath2,
+			Args:   []string{"foo2"},
+			Stdout: os.Stdout,
+		}
+
+		qcr.Run(cmd)
+	}
+
+	// wait for commands to finish
+	<-qcr.Done
+
+	gocleanup.Exit(0)
 }
